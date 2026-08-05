b29cae1341fef3584b268ead9651800aeb4defc0
Vamsy <vamsy.2112@gmail.com>
2026-08-05 22:10:41 +0530

    this and super methods

---
+++ ThisAndSuper.java
@@ -0,0 +1,45 @@
+
+class A {
+    public A() {
+        super(); // a hidden method for each constructor in java.
+        System.out.println("in A");
+    }
+
+    public A(int n) {
+        super();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("in B");
+    }
+
+    // 2. now lets create a parameterised constructor
+    public B(int n) {
+        // super();
+        super(n);
+        System.out.println("in B int");
+    }
+}
+
+public class ThisAndSuper {
+    public static void main() {
+        // B obj = new B(); // 1. this will call sout statements from both A & B classes
+        // since B is
+        // inherited from A.
+
+        // 3. if you make the object creation with parameter then it will call
+        // paramenterised constructor B and main constructor A. Why? because every
+        // constructor has a hidden method called super() which calls the constructor of
+        // parent or super class.
+
+        // 4. Now how to call the parameterised constructor of A? just pass a parameter
+        // to super in B so that it gives a command to call parameterised constructor in
+        // super class.
+
+        B obj = new B(5);
+    }
+}
