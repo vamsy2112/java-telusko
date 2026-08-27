38da677e55756554f0c342a2c647e9b080ff4c7b
Vamsy <vamsy.2112@gmail.com>
2026-08-27 19:02:59 +0530

    ducking the exceptions core

--- DuckingExceptions.java
+++ DuckingExceptions.java
@@ -1,5 +1,30 @@
 public class DuckingExceptions {
+
+    public void a() throws Exception {
+
+    }
+
+    public void b() throws Exception {
+
+    }
+
     public static void main(String[] args) {
         // ducking the exception code goes here...
+
+        try {
+            Class.forName("DuckingExceptions");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found" + e);
+        }
+
+        DuckingExceptions obj = new DuckingExceptions();
+
+        try {
+            obj.a();
+            obj.b();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
