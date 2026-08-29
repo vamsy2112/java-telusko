1fac9f5e40fa9a5265a780410b58bf1c3732aae7
Vamsy <vamsy.2112@gmail.com>
2026-08-29 18:24:07 +0530

    threads in java intro  notes

--- ThreadsInJava.java
+++ ThreadsInJava.java
@@ -1,4 +1,9 @@
 
+//NOTES:
+// 1. if you want to assign new threads, then extend the existing class to Thread class.
+// 2. make sure the methods inside the classes must have name run() only.
+// 3. when you want to call these methods, then ou have to call with obj.start(). see line number 27, 28.
+// 4. and that's it, you've assigned new threads for each of these classes. now it depends on the SCHEDULER to assign the priority among these classes to be executed.
 class Bablu extends Thread {
     public void run() {
         for (int i = 0; i <= 100; i++) {
