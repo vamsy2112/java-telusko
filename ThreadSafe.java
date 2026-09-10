ecf6f9ff98e75c5759a2c4092d5e6dd56ee8a83b
Vamsy <vamsy.2112@gmail.com>
2026-09-11 00:08:44 +0530

    Thread safe with t1.join and synchronised keywords

--- ThreadSafe.java
+++ ThreadSafe.java
@@ -1,28 +1,53 @@
 
-class A implements Runnable {
-    public void run() {
-        int count = 0;
+class A {
+    int count = 0;
 
-        count = count + 1;
-    }
-}
-
-class B implements Runnable {
-    public void run() {
-        int count = 0;
-
-        count = count + 1;
+    public synchronized void increment() {
+        count++;
     }
 }
 
 public class ThreadSafe {
     public static void main(String[] args) {
 
-        // Runnable obj1 = new Runnable();
-        // B obj2 = new B();
+        A obj = new A();
 
-        // obj1.start();
-        // obj2.start();
+        Runnable obj1 = () -> {
+            {
+                for (int i = 0; i < 1000; i++) {
+                    obj.increment();
+                }
+            }
+        };
+
+        Runnable obj2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                obj.increment();
+            }
+
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println(obj.count); // this will not give the correct output because both threads are trying to
+                                       // access the same variable at the same time. so we need to make it thread safe.
 
     }
 }
