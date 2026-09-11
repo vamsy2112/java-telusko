46a42509874d0edd650461ee0ee3c6913055345b
Vamsy <vamsy.2112@gmail.com>
2026-09-11 21:09:47 +0530

    collections api intro

---
+++ Collections_API.java
@@ -0,0 +1,15 @@
+public class Collections_API {
+    public static void main(String[] args) {
+        // NOTES:
+
+        // 1. Collections API is a part of Java's standard library that provides a set
+        // of classes and interfaces for working with collections of objects.
+        // 2. It includes classes for lists, sets, maps, and other data structures
+        // 3. The Collections API provides methods for adding, removing, and
+        // manipulating elements in collections, as well as for sorting and searching
+        // them.
+        // 4. The Collections API is designed to be flexible and extensible, allowing
+        // developers to create their own custom collection classes that can be used
+        // with the standard collection classes.
+    }
+}
