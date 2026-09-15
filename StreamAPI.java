556a68e14d7bb339021b9ff630b4bcd9d7f7c0c9
Vamsy <vamsy.2112@gmail.com>
2026-09-15 23:10:34 +0530

    revision of stream api

--- StreamAPI.java
+++ StreamAPI.java
@@ -6,11 +6,14 @@ public class StreamAPI {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 3, 2, 4, 5, 6, 7, 8, 9, 10);
 
-        Stream<Integer> s1 = nums.stream();
+        Stream<Integer> s1 = nums.stream(); // S1 CAN BE USED ONLY ONCE. CANNOT BE RE-USED. That's the speciality of
+                                            // streams
         Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
         Stream<Integer> s3 = s2.map(n -> n * 2);
         int result = s3.reduce(0, (c, e) -> c + e);
         // s3.forEach(n -> System.out.println(n));
         System.out.println(result);
+
+        // NEED TO DO REVISION OF THESE STREAM API CONCEPTS
     }
 }
